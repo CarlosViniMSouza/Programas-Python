2c4710e0cbd1616da815114b160cdc670dec3ded
CarlosViniMSouza <60679101+CarlosViniMSouza@users.noreply.github.com>
2020-09-29 17:56:14 -0300

    arquivo samsung_ocean_python_avancandonalinguagem

---
+++ Ocean_Py_Advanced.py
@@ -0,0 +1,42 @@
+iniciando a aula:
+
+adm = False #Boolean -> Booleano -> Bool
+print(adm)
+
+ #Comparation - Igual
+
+comp_igual = 1 != 1
+print(comp_igual)
+
+#Sequences of prints:
+print(2!=2) #False
+print(1>=0) #True
+print(5<=5) #True
+
+#Atv:
+print(98 != -98 & 15<=-15) #?
+print(2>=-2) #True
+print(2<=-2) #False
+
+print(not (False and True))
+print(not False and True)
+#see with a '()' changed the result.
+
+print(not (not (True))) #True
+print(not True != (not False) or True == False) #True
+print(True == False and True) #False
+
+print(False != (True or False)) #True
+print(False != (True & False)) #False
+
+print(not (not (True))) #True
+print(not True != (not False) or True==False) #True
+print(True != False or False) #True
+
+# ordem de resolução : not -> and -> or
+
+#If, else and elif:
+adm = 0
+if adm == False:
+    print("Access Completed")
+#pq a saída é "Access Completed"?
